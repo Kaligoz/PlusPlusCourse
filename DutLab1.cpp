447f7027a6c177b81864db54b27cf34ba9efcb64
Глэб Калиниченко <glebkalinichenkolol@gmail.com>
2023-09-08 17:39:34 +0300

    first lab commit

---
+++ DutLab1.cpp
@@ -0,0 +1,19 @@
+﻿    #include <iostream>
+    #include <cmath>
+
+    #define M_PI 3.14159265358979323846
+
+    using namespace std;
+
+    int main()
+    {
+        double a=2, b=12.11, c=3, d=11.05, resultOne, resultTwo;
+
+        resultOne = (0.5 * (log(a) + log(b)) / ( rtsq( cos(sqrt(a+b) + exp(a) ) * (M_PI/6) );
+
+        resultTwo = (1 - cos(sqrt((c) / (d)*M_PI)) / ((0.5 * exp(c)) + (0.312 * exp(d))));
+
+        cout << resultOne << endl;
+        cout << resultTwo;
+        return 0;
+    }
