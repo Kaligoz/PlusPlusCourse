5fe75e02eaf13851a0e64a3e755ba16a5d8d553f
Глэб Калиниченко <glebkalinichenkolol@gmail.com>
2023-09-13 00:00:09 +0300

    second commit found mistakes

--- DutLab1.cpp
+++ DutLab1.cpp
@@ -1,19 +1,22 @@
-﻿    #include <iostream>
-    #include <cmath>
+﻿
+#include <cmath>
+#include <iostream>
 
-    #define M_PI 3.14159265358979323846
+#define M_PI 3.141592653589793288456
 
-    using namespace std;
+using namespace std;
 
-    int main()
-    {
-        double a=2, b=12.11, c=3, d=11.05, resultOne, resultTwo;
+int main()
+{
+    double a = 2, b = 12, c = 3, d = 11.05, resultOne, resultTwo;
 
-        resultOne = (0.5 * (log(a) + log(b)) / ( rtsq( cos(sqrt(a+b) + exp(a) ) * (M_PI/6) );
+    resultOne = (0.5 * (log(a) + log(b))) / sqrt(cos(sqrt(a + b) + exp(a)) * (M_PI / 6)); 
 
-        resultTwo = (1 - cos(sqrt((c) / (d)*M_PI)) / ((0.5 * exp(c)) + (0.312 * exp(d))));
+    resultTwo = (1 - cos(sqrt(c) / d * M_PI)) / ((0.5 * exp(c)) + (0.312 * exp(d))); 
 
-        cout << resultOne << endl;
-        cout << resultTwo;
-        return 0;
-    }
+    cout << resultOne << endl;
+    cout << resultTwo << endl;
+    return 0;
+}
+
+
