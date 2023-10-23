9eb93986b68343d4dac2641237b5181ab31562ff
Kaligoz <114657098+Kaligoz@users.noreply.github.com>
2023-10-23 20:31:39 +0300

    Update DutLab4.cpp

--- DutLab4.cpp
+++ DutLab4.cpp
@@ -71,7 +71,7 @@ void pr4() {
     cin >> num1 >> num2 >> num3 >> num4;
 
     if (num1 >= 1 && num1 <= 255 && 
-        num2 >= 1 && num3 <= 255 &&
+        num2 >= 1 && num2 <= 255 &&
         num3 >= 1 && num3 <= 255 &&
         num4 >= 1 && num4 <= 255) {
         cout << num1 << "." << num2 << "." << num3 << "." << num4 << endl;
